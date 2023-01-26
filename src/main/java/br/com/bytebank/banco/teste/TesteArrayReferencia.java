f74d983e665b248c681cd6a3894c0ff6d26bb2b2
lescarpi <gcleandrinho75@gmail.com>
2023-01-25 22:11:56 -0300

    Testando array de Object e casting

--- src/main/java/br/com/bytebank/banco/teste/TesteArrayReferencia.java
+++ src/main/java/br/com/bytebank/banco/teste/TesteArrayReferencia.java
@@ -1,17 +1,25 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencia {
 
     public static void main(String[] args) {
 
-        ContaCorrente[] contasCorrentes = new ContaCorrente[5];
+        Object[] contas = new Object[5];
 
-        for (int i = 0; i < contasCorrentes.length; i++) {
-            contasCorrentes[i] = new ContaCorrente(i*32+11, i*73+220);
-            System.out.println(contasCorrentes[i].toString());
+        for (int i = 0; i < contas.length; i++) {
+            if (i % 2 == 0) {
+                contas[i] = new ContaPoupanca(i*32+11, i*73+220);
+            } else {
+                contas[i] = new ContaCorrente(i*32+11, i*73+220);
+            }
+            System.out.println(contas[i].toString());
         }
+
+        ContaCorrente ref = (ContaCorrente) contas[1];
 
     }
 
